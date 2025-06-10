88cb4aa99ffa36499186ebb86036d311fd368f8c
liyunfu1998 <liyunfu1998@qq.com>
2025-06-10 15:27:08 +0800

    feat: openai agents 使用

--- 2_openai/deep_research/writer_agent.py
+++ 2_openai/deep_research/writer_agent.py
@@ -12,11 +12,13 @@ INSTRUCTIONS = (
 
 
 class ReportData(BaseModel):
-    short_summary: str = Field(description="A short 2-3 sentence summary of the findings.")
+    short_summary: str = Field(
+        description="A short 2-3 sentence summary of the findings.")
 
     markdown_report: str = Field(description="The final report")
 
-    follow_up_questions: list[str] = Field(description="Suggested topics to research further")
+    follow_up_questions: list[str] = Field(
+        description="Suggested topics to research further")
 
 
 writer_agent = Agent(
