020ba9a2f7cd2913619bb72b47c7bda68d5a65ac
liyunfu1998 <liyunfu1998@qq.com>
2025-06-10 19:32:25 +0800

    feat: 支持code interpretor

---
+++ 3_crew/no4_coder/src/no4_coder/crew.py
@@ -0,0 +1,43 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+
+
+@CrewBase
+class No4Coder():
+    """No4Coder crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    @agent
+    def coder(self) -> Agent:
+        return Agent(
+            config=self.agents_config['coder'],
+            verbose=True,
+            allow_code_execution=True,
+            code_execution_mode="safe",
+            max_execution_time=30,
+            max_retry_limit=5
+        )
+
+    @task
+    def coding_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['coding_task'],
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the No4Coder crew"""
+        # To learn how to add knowledge sources to your crew, check out the documentation:
+        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
